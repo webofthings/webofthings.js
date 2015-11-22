b6165fe0591fbbef60924051f3a241fe0d04f9cd
Dom Guinard <dev@guinard.org>
2015-11-22 18:59:03 +0000

    Refactoring, cleaning

--- plugins/internal/pirPlugin.js
+++ plugins/internal/pirPlugin.js
@@ -30,8 +30,8 @@ PirPlugin.prototype.connectHardware = function () {
   sensor = new Gpio(self.model.values.presence.customFields.gpio, 'in', 'both');
   sensor.watch(function (err, value) {
     if (err) exit(err);
-    this.showValue();
-    this.addValue(!!value);
+    self.showValue();
+    self.addValue(!!value);
   });
   console.info('Hardware %s sensor started!', self.model.name);
 };
